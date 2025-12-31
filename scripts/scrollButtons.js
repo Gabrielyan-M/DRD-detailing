a885d9cf715c2e378979ef3a309e554b1d103ea3
Gabrielyan-M <gabrielyan.m77@gmail.com>
2026-01-01 02:29:39 +0500

    Add files via upload

--- scripts/scrollButtons.js
+++ scripts/scrollButtons.js
@@ -4,22 +4,37 @@ const welcomeBlock = document.querySelector('[data-js-welcome-block]')
 scrollButtom.forEach(button => {
       button.addEventListener('click', (e) => {
 
-
             button.getAttribute('data-js-scroll-button') === 'bottom' 
             ? window.scrollTo({top: welcomeBlock.clientHeight, behavior: 'smooth'})
             : null
-
 
       })
 })
 
 
-// welcome block
-
+// resize welcome block
 const homeBlock = document.querySelector('[data-js-welcome-block]')
 
 window.addEventListener('scroll', () => {
       window.pageYOffset < window.innerHeight
       ? homeBlock.style.transform = `translateY(-${window.pageYOffset/8}px)`
       : null
+})
+
+
+// present button
+const presentButton = document.querySelector('[data-js-present-button]')
+const formBlock = document.querySelector('[data-js-form-block]')
+
+presentButton.addEventListener('click', () => {
+      
+      
+
+
+      window.scrollTo({
+            top: formBlock.scrollTop - (formBlock.clientHeight/2),
+            behavior: 'smooth'
+      })
+
+      
 })
