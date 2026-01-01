2267b4d213c65cd7d386cfcab6d7fc91c330533b
Gabrielyan-M <gabrielyan.m77@gmail.com>
2026-01-01 23:06:27 +0500

    Add files via upload

--- scripts/scrollButtons.js
+++ scripts/scrollButtons.js
@@ -1,40 +1,33 @@
 const scrollButtom = document.querySelectorAll('[data-js-scroll-button]')
 const welcomeBlock = document.querySelector('[data-js-welcome-block]')
 
+const homeBlock = document.querySelector('[data-js-welcome-block]')
+
+const formBlock = document.querySelector('[data-js-form-block]')
+
+
 scrollButtom.forEach(button => {
       button.addEventListener('click', (e) => {
 
             button.getAttribute('data-js-scroll-button') === 'bottom' 
-            ? window.scrollTo({top: welcomeBlock.clientHeight, behavior: 'smooth'})
+            ? window.scrollTo({ top: welcomeBlock.clientHeight, behavior: 'smooth' })
             : null
 
+            button.getAttribute('data-js-scroll-button') === 'top' 
+            ? window.scrollTo({ top: welcomeBlock.getBoundingClientRect().y, behavior: 'smooth' })
+            : null
+
+            button.getAttribute('data-js-scroll-button') === 'form' 
+            ? window.scrollTo({ top: formBlock.getBoundingClientRect().y + formBlock.clientHeight/2 - window.innerHeight/2, behavior: 'smooth' })
+            : null
       })
 })
 
 
-// resize welcome block
-const homeBlock = document.querySelector('[data-js-welcome-block]')
 
+// resize welcome block
 window.addEventListener('scroll', () => {
       window.pageYOffset < window.innerHeight
       ? homeBlock.style.transform = `translateY(-${window.pageYOffset/8}px)`
       : null
-})
-
-
-// present button
-const presentButton = document.querySelector('[data-js-present-button]')
-const formBlock = document.querySelector('[data-js-form-block]')
-
-presentButton.addEventListener('click', () => {
-      
-      
-
-
-      window.scrollTo({
-            top: formBlock.scrollTop - (formBlock.clientHeight/2),
-            behavior: 'smooth'
-      })
-
-      
 })
