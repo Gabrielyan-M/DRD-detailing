20def74489eec930080aafff90da67c670994c11
Gabrielyan-M <gabrielyan.m77@gmail.com>
2026-01-05 21:38:11 +0500

    Add files via upload

--- scripts/openPage.js
+++ scripts/openPage.js
@@ -16,4 +16,24 @@ function openPage (cardID) {
       cardID === '002'
       ? window.location.assign('./service-pages/noiseInsulation.html')
       : null
+
+      cardID === '003'
+      ? window.location.assign('./service-pages/motorWashing.html')
+      : null
+
+      cardID === '004'
+      ? window.location.assign('./service-pages/ozonation.html')
+      : null
+
+      cardID === '005'
+      ? window.location.assign('./service-pages/headlightRestoration.html')
+      : null
+
+      cardID === '006'
+      ? window.location.assign('./service-pages/antiRain.html')
+      : null
+
+      cardID === '007'
+      ? window.location.assign('./service-pages/polishing.html')
+      : null
 }
