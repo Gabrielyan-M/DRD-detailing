9bf0515a9b7f6e72a50d0c66e55bf7e98932d0c4
Gabrielyan-M <gabrielyan.m77@gmail.com>
2026-01-07 15:50:41 +0500

    fixed-phone-button.js

--- scripts/fixed-phone-button.js
+++ scripts/fixed-phone-button.js
@@ -4,9 +4,10 @@ const equipmentLine = document.querySelector('[data-js-equipment-line-top]')
 window.addEventListener('scroll', () => {
      
 
-      if ( ( equipmentLine.getBoundingClientRect().top - window.innerHeight + 30 ) < 0 )  {
+      if ( ( equipmentLine.getBoundingClientRect().top - window.innerHeight - 20 ) < 0 )  {
             fixedPhoneButton.style.display = "none"
       } else {
             fixedPhoneButton.style.display = "flex"
       }
+
 })
