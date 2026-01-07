04ac18fae4f0f0bcb88ca3dc79ecb93f27d82bbc
Gabrielyan-M <gabrielyan.m77@gmail.com>
2026-01-07 16:00:53 +0500

    Add files via upload

--- scripts/fixed-phone-button.js
+++ scripts/fixed-phone-button.js
@@ -4,10 +4,9 @@ const equipmentLine = document.querySelector('[data-js-equipment-line-top]')
 window.addEventListener('scroll', () => {
      
 
-      if ( ( equipmentLine.getBoundingClientRect().top - window.innerHeight - 20 ) < 0 )  {
+      if ( ( equipmentLine.getBoundingClientRect().top - window.innerHeight + 30 ) < 0 )  {
             fixedPhoneButton.style.display = "none"
       } else {
             fixedPhoneButton.style.display = "flex"
       }
-
 })
