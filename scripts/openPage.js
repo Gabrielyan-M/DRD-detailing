a7697f74d0571338e09223c8253d8407a0053c23
Gabrielyan-M <gabrielyan.m77@gmail.com>
2026-01-07 16:31:50 +0500

    Add files via upload

--- scripts/openPage.js
+++ scripts/openPage.js
@@ -3,6 +3,9 @@ const cardServise = document.querySelectorAll('[data-js-card-servises]')
 cardServise.forEach(card => (
       card.addEventListener('click', () => {
             const cardID = card.getAttribute('data-js-card-servises')
+
+            console.log(card);
+            
             
             if (cardID) {
                   openPage (cardID)
@@ -47,6 +50,10 @@ function openPage (cardID) {
                   pageLink = './service-pages/tinting.html'
                   break
 
+            case 'mainForm':
+                  pageLink = '../index.html#formBlock'
+                  break
+
 
             default:
                   return
@@ -57,33 +64,4 @@ function openPage (cardID) {
 
 
 
-
-
-      // cardID === '001'
-      // ? window.location.assign('./service-pages/dryCleaning.html')
-      // : null
-
-      // cardID === '002'
-      // ? window.location.assign('./service-pages/noiseInsulation.html')
-      // : null
-
-      // cardID === '003'
-      // ? window.location.assign('./service-pages/motorWashing.html')
-      // : null
-
-      // cardID === '004'
-      // ? window.location.assign('./service-pages/ozonation.html')
-      // : null
-
-      // cardID === '005'
-      // ? window.location.assign('./service-pages/headlightRestoration.html')
-      // : null
-
-      // cardID === '006'
-      // ? window.location.assign('./service-pages/antiRain.html')
-      // : null
-
-      // cardID === '007'
-      // ? window.location.assign('./service-pages/polishing.html')
-      // : null
 }
