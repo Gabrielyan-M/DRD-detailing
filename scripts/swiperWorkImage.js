6d634027b8ba79a87d8724bcb043b92d1fd3050b
Gabrielyan-M <gabrielyan.m77@gmail.com>
2026-01-08 23:01:46 +0500

    Add files via upload

--- scripts/swiperWorkImage.js
+++ scripts/swiperWorkImage.js
@@ -1,613 +1,68 @@
-const sliderWork = document.querySelectorAll('[data-js-examples-work-images-container]')
-
-const workImageLeft = document.querySelectorAll('[data-js-work-image-left]')
-const leftImg = document.querySelectorAll('[data-js-work-image-left-img]')
-
-const scrollLineBlock = document.querySelectorAll('[data-js-scroll-line-block]')
+const imagesWorkContainer = document.querySelectorAll('[data-js-examples-work-images-container]')
+const imageLeftBlock = document.querySelectorAll('[data-js-work-image-left]')
+const scrollLine = document.querySelectorAll('[data-js-scroll-line-block]')
 
 
-let isActive = false
+let isActiveArr = []
 
-sliderWork.forEach((slider, index) => {
-      
-      slider.addEventListener('mousemove', (e) => {
-            if ( isActive ) {
-                  scrollLineBlock[index].style.left = `${e.offsetX / slider.offsetWidth * 100}%`
-                  leftImg.style.width = `${e.offsetX / slider.offsetWidth * 100}%`
-            }
-
-            // console.log(e);
-            
-      })
-
-      slider.addEventListener('mouseleave', (e) => {
-            isActive = false
-      })
-
-      scrollLineBlock[index].addEventListener('mousedown', (e) => {
-            isActive = true
-      })
-
-      scrollLineBlock[index].addEventListener('mouseup', (e) => {
-            isActive = false
-      })
-    
-
-
-
-
-
-
-
-
-
-
-
+document.addEventListener('DOMContentLoaded', () => {
+      for (let y = 0; y < imagesWorkContainer.length; y++) {
+            isActiveArr = [...isActiveArr, false]
+      }
 })
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// sliderWork.forEach(slider => {
-//       slider.addEventListener('mousemove', (e) => {
-//             isActive 
-//             ? scrollMouse (e)
-//             : null 
-//       })
-// })
-
-// scrollLineBlock.forEach(block => {
-//       block.addEventListener('mousedown', () => {
-//             isActive = true
-//       })
-// })
-
-// scrollLineBlock.forEach(block => {
-//       block.addEventListener('mouseup', () => {
-//             isActive = false
-//       })
-// })
-
-// scrollLineBlock.forEach(block => {
-//       block.addEventListener('touchmove', (e) => {
-//             isActive 
-//             ? scrollTouch (e)
-//             : null 
-//       })
-// })
-
-// scrollLineBlock.forEach(block => {
-//       block.addEventListener('touchstart', () => {
-//             isActive = true
-//       })
-// })
-
-// scrollLineBlock.forEach(block => {
-//       block.addEventListener('touchend', () => {
-//             isActive = false
-//       })
-// })
-
-
-// function scrollMouse (e) {
-//       let x = e.clientX / sliderWork.offsetWidth * 100
-
-//       e.target.style.left = `${x}%`
-//       // workImageLeft.style.width = `${x}%`
+const beforeAfterSlider = (x, i) => {
+      let percent = Math.max(0, Math.min(x, imagesWorkContainer[i].getBoundingClientRect().width))
+      scrollLine[i].style.left = `${percent}px`
+      imageLeftBlock[i].style.width = `${percent}px`
+}
+
+imagesWorkContainer.forEach((container, index) => {
+      container.addEventListener('pointermove', (e) => {
+            if ( !isActiveArr[index] ) return
+            const x = Math.max(0, e.clientX - container.getBoundingClientRect().left) 
+            beforeAfterSlider(x, index)
+
+            console.log(e);
+            
+      });
+
+      container.addEventListener('mouseleave', (e) => {
+            isActiveArr[index] = false
+      });
+
+            
+
+});
+
+scrollLine.forEach((line, index) => {
+
+      line.addEventListener('mousedown', (e) => {
+            isActiveArr[index] = true
+      })
       
-// }
+      line.addEventListener('mouseup', (e) => {
+            isActiveArr[index] = false
+      })
 
-// function scrollTouch (e) {
-//       let x = e.changedTouches[0].pageX 
+      line.addEventListener('touchstart', () => {
+            isActiveArr[index] = true
+      })
+
+      line.addEventListener('touchend', () => {
+            isActiveArr[index] = true
+      })
+})
+
+
+
+// imagesWorkContainerTWO.addEventListener('mousemove', (e) => {
+//       // if ( !isActive ) return
+
+//       let x = e.offsetX
+//       console.log(x);
       
-//       e.target.style.left = `${x/sliderWork.offsetWidth * 100}%`
-//       // workImageLeft.style.width = `${x/sliderWork.offsetWidth * 100}%`   
-// }
+// })
+
