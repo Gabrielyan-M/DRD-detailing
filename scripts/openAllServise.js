715cbc232018b20932068b873173853df4f6e05b
Gabrielyan-M <gabrielyan.m77@gmail.com>
2026-01-10 15:53:46 +0500

    Add files via upload

--- scripts/openAllServise.js
+++ scripts/openAllServise.js
@@ -1,10 +1,13 @@
 const allServiseButton = document.querySelector('[data-js-all-servise-button]')
 const cardsServiseBlock = document.querySelector('[data-js-cards-servise-block]')
 
-if (window.innerHeight <= 740) {
-      allServiseButton.addEventListener('click', openListServise )
+let windowY = window.pageYOffset
 
-      function openListServise(e) {
+allServiseButton.addEventListener('click', openListServise )
+
+function openListServise(e) {
+      if (window.innerHeight <= 740) {
+
             
             let h = cardsServiseBlock.children[0].getBoundingClientRect().height*2 + 20
             
@@ -12,8 +15,16 @@ if (window.innerHeight <= 740) {
             ?e.target.innerText = 'Скрыть список'
             :e.target.innerText = 'Все услуги'
 
-            cardsServiseBlock.clientHeight !== cardsServiseBlock.scrollHeight
-            ?cardsServiseBlock.style.height = `${cardsServiseBlock.scrollHeight}px`   
-            :cardsServiseBlock.style.height = `${h}px`   
+            if ( cardsServiseBlock.clientHeight !== cardsServiseBlock.scrollHeight ) {                  
+                  cardsServiseBlock.style.height = `${cardsServiseBlock.scrollHeight}px`                    
+            } else {
+                  cardsServiseBlock.style.height = `${h}px` 
+                  console.log(windowY);
+                  
+                  window.scrollTo({ top: windowY, behavior: 'instant' })
+            }
+
+            windowY = window.pageYOffset
+
       }
 }
