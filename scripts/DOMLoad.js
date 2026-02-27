05da3ea030406975dc97c7a32f47b719c477fbc7
Gabrielyan-M <gabrielyan.m77@gmail.com>
2026-02-27 23:12:21 +0500

    Delete scripts directory

--- scripts/DOMLoad.js
+++
@@ -1,7 +0,0 @@
-import { scrollToForm } from './scrollButtons.js'
-
-window.addEventListener('load', () => {
-      if (location.hash === '#formBlock' ) {
-            scrollToForm ()
-      }
-})
--- scripts/scrollWindowMain.js
+++
@@ -1,7 +0,0 @@
-import { visibleHiddenBurgerButton } from "./burgerMenu.js";
-import { fixedPhone } from "./fixed-phone-button.js";
-
-window.addEventListener('scroll', () => {
-      visibleHiddenBurgerButton ()
-      fixedPhone ()
-})
